f3e5493877bd00528aff00f728b5b04e8574a2bd
Magic-B <majorheby@gmail.com>
2022-12-07 00:29:06 +0300

    fixed bugs 2.0

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const aloneBtn = document.querySelector('#alone');
 const compBtn = document.querySelector('#computer');
 
 const winPos = [
-  [1, 2, 3], 
+  [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
   [1, 4, 7],
@@ -16,6 +16,7 @@ const winPos = [
 ];
 
 let start = false;
+let blocked = false;
 
 let x = [];
 let o = [];
@@ -24,10 +25,13 @@ let whoWalks = 'X';
 
 aloneBtn.addEventListener('click', () => {
   startGame();
+  new Audio('./audio/start.mp3').play();
 });
 
 boxes.addEventListener('click', (e) => {
-  if (e.target.id && start) {
+  if (blocked) return
+
+  if (e.target.id && e.target.id !== 'game' && start) {
     if(whoWalks == 'X') {
       stepFor(e.target, x, 'X', 'O', '#00E0AA');
     } else if (whoWalks == 'O') {
@@ -44,6 +48,8 @@ function startGame() {
 }
 
 function stepFor(target, arr, whom, next, color) {
+  const clickAudio =  new Audio('./audio/138c15880ef929c.mp3');
+
   if (x.includes(+target.id) || o.includes(+target.id)) {
     openModal('This position is occupied!');
     whoWalks = whom;
@@ -55,15 +61,17 @@ function stepFor(target, arr, whom, next, color) {
     whoWalks = next;
     target.textContent += whom;
     target.style = `color: ${color}`;
+    clickAudio.play();
   }
 
-  if (isWin(arr) == true) {
-    alert(`${whom} is win`);
-    gameOver();
+  const result = isWin(arr);
+
+  if (result === 'draw') {
+    win();
   }
-  if (isWin(arr) == 'draw') {
-    alert(`this is draw`);
-    gameOver();
+
+  if (result && result !== 'draw') {
+    win(whom, result);
   }
 }
 
@@ -71,9 +79,48 @@ function isWin(user) {
   if(user.length >= 5) {
     return 'draw';
   }
-  return winPos.some((pos) => {
+
+  let combination = null;
+  const win = winPos.some((pos) => {
+    combination =  pos;
     return pos.every((p) => user.includes(p));
   });
+
+  return win ? combination : false;
+}
+
+function win(winner = null, combination = null) {
+  const gifs = document.querySelectorAll('.gif');
+  const winDiv = document.createElement('div');
+
+  if (winner) {
+    winDiv.textContent = `${winner} is win`;
+    document.body.prepend(winDiv);
+    winDiv.classList = 'winner';
+    gifs[0].classList.remove('fadeOutLeft');
+    gifs[1].classList.remove('fadeOutRight');
+    gifs[0].classList.add('bounceInLeftCat');
+    gifs[1].classList.add('bounceInRightCat');
+    new Audio('./audio/gameOver.mp3').play();
+    
+  } else {
+    openModal('This is draw');
+  }
+  blocked = true;
+  const game = document.querySelector('#game');
+  if (combination) {
+    game.classList.add(`win_${combination.join('')}`);
+  }
+
+  setTimeout(() => {
+    gameOver();
+    winDiv.remove();
+    if (combination) {
+      game.classList.remove(`win_${combination.join('')}`);
+      gifs[0].classList.add('fadeOutLeft');
+      gifs[1].classList.add('fadeOutRight');
+    }
+  }, 2000);
 }
 
 function gameOver() {
@@ -81,6 +128,7 @@ function gameOver() {
   o = [];
   document.querySelectorAll('.game-box').forEach((item) => item.textContent = '');
   whoWalks = 'X';
+  blocked = false;
 }
 
 // Modal
@@ -108,14 +156,14 @@ function openModal(title) {
 modalCloseBtn.addEventListener('click', closeModal);
 okeyBtn.addEventListener('click', closeModal);
 
-modal.addEventListener('click', (e) => {
+modal.addEventListener('click', e => {
   if (e.target === modal) {
       closeModal();
   }
 });
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === "Escape" && modal.classList.contains('show')) { 
+document.addEventListener('keydown', e => {
+  if (e.code === "Escape" && modal.classList.contains('show')) {
       closeModal();
   }
 });
